d28d968f58dd15422a2012d211a29f5dd981f010
md-mehedi-hasan-nabil <mehedihasannabil49@gmail.com>
2024-11-12 22:45:29 +0600

    load data from remote server

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.index, name='index'),
+    path('post/<str:postId>/', views.post, name='post'),
     path('about', views.about, name='about'),
     path('contact-us', views.contact, name='contact-us'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import http.client
+import json
 # Create your views here.
-def home(request):
-    return render(request, 'first_app/home.html')
+
+
+def index(request):
+    conn = http.client.HTTPSConnection("jsonplaceholder.typicode.com")
+    conn.request("GET", "/posts")
+    response = conn.getresponse()
+
+    data = response.read()
+    parsed_data = json.loads(data)
+
+    conn.close()
+
+    context = {"posts": parsed_data}
+
+    return render(request, 'first_app/index.html', context)
+
+
+def post(request, postId):
+    conn = http.client.HTTPSConnection("jsonplaceholder.typicode.com")
+    conn.request("GET", f"/posts/{postId}")
+    response = conn.getresponse()
+
+    data = response.read()
+    parsed_data = json.loads(data)
+
+    conn.close()
+
+    context = {"post": parsed_data}
+
+    return render(request, "first_app/post.html", context)
+
 
 def contact(request):
     return render(request, 'first_app/contact.html')
+
 
 def about(request):
     return render(request, 'first_app/about.html')
